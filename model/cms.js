21315e7e0bb4030de8490746120b653a598b1a5d
Daniel Montgomery <d.montgrmry@gmail.com>
2017-08-01 16:48:06 -0700

    Refactor cms into serprate collections

--- model/cms.js
+++ model/cms.js
@@ -1,8 +1,36 @@
 Cms = new Mongo.Collection("cms");
+Guides = new Mongo.Collection("guides");
+Articles = new Mongo.Collection("articles");
 
 
 //for now - just being logged in should be good enough
 Cms.allow({
+    insert: function(userId) {
+        return userId;
+    },
+    update: function(userId) {
+        return userId;
+    },
+    remove: function(userId) {
+        return userId;
+    }
+});
+
+//for now - just being logged in should be good enough
+Guides.allow({
+    insert: function(userId) {
+        return userId;
+    },
+    update: function(userId) {
+        return userId;
+    },
+    remove: function(userId) {
+        return userId;
+    }
+});
+
+//for now - just being logged in should be good enough
+Articles.allow({
     insert: function(userId) {
         return userId;
     },
--- server/methods/cmsMethods.js
+++ server/methods/cmsMethods.js
@@ -1,7 +1,13 @@
 Meteor.publish('cms', function() {
-  return Cms.find({
-    cmsType: 'room'
-  });
+  return Cms.find({});
+});
+
+Meteor.publish('guides', function() {
+    return Guides.find({});
+});
+
+Meteor.publish('articles', function() {
+    return Articles.find({});
 });
 
 Meteor.methods({
@@ -19,39 +25,13 @@ Meteor.methods({
       console.log("Adding new guide room...");
 
 
-      //add auto data and remove $$hashKey
+      //add auto data
 
       /* ROOM */
       newRoom.createdById = Meteor.userId();
       newRoom.creationDate = new Date();
       newRoom.modifiedById = Meteor.userId();
       newRoom.modifiedDate = new Date();
-
-      /* GUIDES */
-
-      newRoom.guides.forEach((guide) => {
-        // add data
-        guide.createdById = Meteor.userId();
-        guide.creationDate = new Date();
-        guide.modifiedById = Meteor.userId();
-        guide.modifiedDate = new Date();
-        
-        // remove $$hashKey
-        delete guide.$$hashKey;
-        
-        /* ARTICLES */
-        
-        guide.articles.forEach((article) => {
-          // add data
-          article.createdById = Meteor.userId();
-          article.creationDate = new Date();
-          article.modifiedById = Meteor.userId();
-          article.modifiedDate = new Date();
-
-          //remove $$hashKey
-          delete article.$$hashKey;
-        });
-      });
 
       return Cms.insert(newRoom, (err, data) => {
           if (err)
@@ -60,6 +40,71 @@ Meteor.methods({
               console.log('New Guide Room Created: ' + data);
           }
       });
+    },
 
+    createGuide: function (newGuide) {
+      // Check privilege
+      if (!Roles.userIsInRole(Meteor.userId(),
+              ['superAdmin',
+                  'editGuide',
+                  'editAllGuides'
+              ])) {
+          throw new Meteor.Error("Insufficient Privilege to add new Guide.")
+      }
+
+      console.log("Adding new guide...");
+
+
+      //add auto data
+
+      /* GUIDES */
+      newGuide.createdById = Meteor.userId();
+      newGuide.creationDate = new Date();
+      newGuide.modifiedById = Meteor.userId();
+      newGuide.modifiedDate = new Date();
+      
+      // remove $$hashKey
+      delete newGuide.$$hashKey;
+
+      return Guides.insert(newGuide, (err, data) => {
+          if (err)
+              console.log('Error adding new guide to db: ' + err);
+          else {
+              console.log('New Guide Created: ' + data);
+          }
+      });
+    },
+
+    createArticle: function (newArticle) {
+      // Check privilege
+      if (!Roles.userIsInRole(Meteor.userId(),
+              ['superAdmin',
+                  'editArticle',
+                  'editAllArticles'
+              ])) {
+          throw new Meteor.Error("Insufficient Privilege to add new Article.")
+      }
+
+      console.log("Adding new article...");
+
+
+      //add auto data
+
+      /* ARTICLES */
+      newArticle.createdById = Meteor.userId();
+      newArticle.creationDate = new Date();
+      newArticle.modifiedById = Meteor.userId();
+      newArticle.modifiedDate = new Date();
+      
+      // remove $$hashKey
+      delete newArticle.$$hashKey;
+
+      return Articles.insert(newArticle, (err, data) => {
+          if (err)
+              console.log('Error adding new article to db: ' + err);
+          else {
+              console.log('New Article Created: ' + data);
+          }
+      });
     }
   });
