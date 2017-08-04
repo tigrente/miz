e7c15147147b3cd439de8da67a306fae49033d82
Daniel Montgomery <d.montgrmry@gmail.com>
2017-08-03 21:40:01 -0700

    Add guide admin component

---
+++ client/guides/components/admin/guide-admin/guide-admin.component.js
@@ -0,0 +1,38 @@
+miz.directive("guideAdmin", function ($compile) {
+  return {
+      restrict: 'E',
+      templateUrl: 'client/guides/components/admin/guide-admin/guide-admin.ng.html',
+      controllerAs: 'ga',
+      controller: function ($scope, $reactive, $stateParams) {
+
+          $reactive(this).attach($scope);
+
+          /* HELPERS */
+          this.helpers({
+            guide: () => {
+              return Guides.find({
+                _id: $stateParams.guideId
+              });
+            }
+          });
+
+
+          /* SUBSCRIPTIONS */
+          this.subscribe('guides');
+
+          /* AUTORUN*/
+          this.autorun(() => {
+
+          }); //autorun
+
+          /* FUNCTIONS */
+          this.incNewArticles = function() {
+            this.newArticles++;
+          }
+
+          /* INITIALIZE */
+          this.newArticles = 0;
+
+      } // controller
+  };  //return
+});
