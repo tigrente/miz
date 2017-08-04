55079140614ae98b0b22d59361e23ec32eb8f2f3
Daniel Montgomery <d.montgrmry@gmail.com>
2017-08-03 19:38:56 -0700

    Refactor code and add (broken) confirmation delete modal.

--- client/guides/components/info/guide-info/guide-info.component.js
+++ client/guides/components/info/guide-info/guide-info.component.js
@@ -1,58 +1,57 @@
 miz.directive("guideInfo", function () {
   return {
-      restrict: 'E',
-      templateUrl: 'client/guides/components/info/guide-info/guide-info.ng.html',
-      controllerAs: 'ge',
-      bindToController: {
-        guideId: '<',
-        roomId: '<'
-      },
-      controller: function ($scope, $reactive) {
+    restrict: 'E',
+    templateUrl: 'client/guides/components/info/guide-info/guide-info.ng.html',
+    controllerAs: 'gi',
+    bindToController: {
+      guideId: '<',
+      roomId: '<'
+    },
+    controller: function ($scope, $reactive) {
 
-          $reactive(this).attach($scope);
+      $reactive(this).attach($scope);
 
-          /* HELPERS */
-          this.helpers({
-            guide: () => {
-              return Guides.findOne({
-                _id: this.guideId
-              });
-            }
+      /* HELPERS */
+      this.helpers({
+        guide: () => {
+          return Guides.findOne({
+            _id: this.guideId
           });
+        }
+      });
+      
+      /* AUTORUN*/
+      this.autorun(() => {
 
+      }); //autorun
 
-          /* SUBSCRIPTIONS */ 
-          this.subscribe('guides');
-          
-          /* FUNCTIONS */
+      /* SUBSCRIPTIONS */ 
+      this.subscribe('guides');
+      
+      /* FUNCTIONS */
 
-          /* INITIALIZE */
-          this.editGuide = function() {
-            console.log('Editing guide..')
+      this.editGuide = function() {
+        console.log('Editing guide..')
+      }
+      
+      this.delete = function() {
+        this.call('deleteGuide', this.guideId, (err, data) => {
+          if (err) {
+            alert('Something went wrong deleting a guide: ' + err);
+          } else {
+            
           }
-
-          this.delete = function() {
-            this.call('deleteGuide', this.guideId, (err, data) => {
-              if (err) {
-                alert('Something went wrong deleting a guide: ' + err);
-              } else {
-                
-              }
-            });
-
-            this.call('updateParentGuideRoomWithDeletedChildGuideId', this.guideId, this.roomId, (err) => {
-              if (err) {
-                alert('Something went wrong trying to update the parent guide room: ' + err);
-              }
-            });
+        });
+        
+        this.call('updateParentGuideRoomWithDeletedChildGuideId', this.guideId, this.roomId, (err) => {
+          if (err) {
+            alert('Something went wrong trying to update the parent guide room: ' + err);
           }
+        });
+      }
+        
+      /* INITIALIZE */
 
-          /* AUTORUN*/
-          this.autorun(() => {
-
-          }); //autorun
-
-
-      } // controller
+    } // controller
   };  //return
 });
