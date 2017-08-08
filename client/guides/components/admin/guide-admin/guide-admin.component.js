ea2c55894fdbf70d4252233808588c9ce2a68908
Daniel Montgomery <d.montgrmry@gmail.com>
2017-08-08 16:04:58 -0700

    Refactor multiple components into few ones

--- client/guides/components/admin/guide-admin/guide-admin.component.js
+++ client/guides/components/admin/guide-admin/guide-admin.component.js
@@ -14,15 +14,29 @@ miz.directive("guideAdmin", function ($compile) {
                 _id: $stateParams.guideId
               });
             },
+
             editedGuide: () => {
               this.getReactively('guide');
               return _.clone(this.guide);
+            },
+
+            articles: () => {
+              this.getReactively('guide');
+
+              if (this.guide) {
+                return Articles.find({
+                  _id: {
+                      $in: this.guide.childrenArticleIds
+                  }
+                });
+              }
             }
           });
 
 
           /* SUBSCRIPTIONS */
           this.subscribe('guides');
+          this.subscribe('articles');
 
           /* AUTORUN*/
           this.autorun(() => {
@@ -43,10 +57,45 @@ miz.directive("guideAdmin", function ($compile) {
             this.reset();
           }
 
+          this.resetNewArticle = function() {
+            this.newArticle = {
+              'cmsType': 'article',
+              'articleType': 'blogWithFiles', // for now, hard code...
+              'publish': 'preview',
+              'articleImage': null, // for later...
+              'files': [], // for later...
+              'title': '',
+              'body': "<i>This is a auto-generated body stub. Click here to edit!</i>"
+              // Other data added when inserting into DB
+            };
+          }
+
           this.addNewArticle = function() {
+            this.resetNewArticle();
+
             $('#new-articles').append(
-              $compile("<article-create guide='ga.guide'></article-create>")($scope)
+              $compile(`
+                <tr>
+                <td> <a href="#" editable-text="ga.newArticle.title" e-label="Article Title">{{ ga.newArticle.title || 'Article Name' }}</a> </td>
+                <td> - </td>
+                <td> - </td>
+                <td> 
+                  <a href="#" editable-select="ga.newArticle.publish" e-ng-options="s.value as s.text for s in ga.xEditableVisStatuses" buttons="no">
+                    {{ ga.showXEditableVisStatus() }}
+                  </a>
+                </td>
+                <td> <button type="button" class="btn btn-primary" ng-click="ga.submitNewArticle($event)" ng-disabled="!ga.canSubmitNewArticleFn()">Save</button> </td>
+                <td> <i class="fa fa-arrow-up" aria-hidden="true"></i> </td>       
+                <td> <i class="fa fa-arrow-down" aria-hidden="true"></i> </td>      
+                <td> <i class="fa fa-times remove" aria-hidden="true" style="color:red;cursor:pointer" ng-click="ga.removeNewArticle($event)"></i> </td>
+                </tr>
+              `)($scope)
             );
+          }
+
+          this.canAddNewArticle = function() {
+            this.getReactively('newArticle');
+            return _.size(this.newArticle) !== 0;
           }
 
           this.reset = function() {
@@ -63,8 +112,71 @@ miz.directive("guideAdmin", function ($compile) {
             return this.guideChanged || false;
         }
 
+        this.editArticle = function(articleId) {
+          window.location.href = '/article/' + articleId + '/edit';
+        }
+
+        this.deleteArticle = function() {
+          if (this.articleIdToDelete !== -1) {
+            this.call('deleteArticle', this.articleIdToDelete, (err) => {
+              if (err) {
+                alert('Something went wrong deleting an article: ' + err);
+              }
+            });
+            
+            this.call('updateParentGuideWithDeletedChildArticleId', this.articleId, this.guideId, (err) => {
+              if (err) {
+                alert('Something went wrong trying to update the parent guide: ' + err);
+              }
+            });
+          } else {
+            alert('article ID not found, cannot delete. Reseting article ID.');
+          }
+
+          this.articleIdToDelete = -1;
+        }
+
+        this.submitNewArticle = function($event) {
+          this.call('createArticle', this.newArticle, (err, data) => {
+            if (err) {
+              alert('Something went wrong adding a new article to database: ' + err);
+            } else {
+              this.call('updateParentGuideWithChildArticleId', data, this.guide._id, (err) => {
+                if (err) {
+                  alert('Something went wrong trying to update the parent guide room: ' + err);
+                }
+              });
+            }
+          });
+
+          this.removeNewArticle($event);
+        }
+
+        this.removeNewArticle = function($event) {
+          $event.target.closest('tr').remove();
+          this.newArticle = {};
+        }
+
+        this.canSubmitNewArticleFn = function() {
+          // check that article has require properties
+          return this.newArticle.title && this.newArticle.body;
+        };
+
+        this.showXEditableVisStatus = function() {
+          selected = _.where(this.xEditableVisStatuses, { value: this.newArticle.publish });
+          return (this.newArticle.publish && selected.length) ? selected[0].text : 'Not set';
+        }
+
         /* INITIALIZE */
         this.guideChanged = false;
+        this.articleIdToDelete = -1;
+
+        this.newArticle = {};
+        this.xEditableVisStatuses = [
+          { value: 'publish', text: 'Publish'},
+          { value: 'preview', text: 'Preview'},
+          { value: 'hide', text: 'Hide'}
+        ]
 
       } // controller
   };  //return
--- client/guides/components/admin/room-admin/room-admin.component.js
+++ client/guides/components/admin/room-admin/room-admin.component.js
@@ -11,27 +11,140 @@ miz.directive("guideRoomAdmin", function ($compile) {
           this.helpers({
             rooms: () => {
               return Cms.find({});
+            },
+
+            currentRoom: () => {
+              return Cms.findOne();
+            },
+
+            guides: () => {
+              this.getReactively('currentRoom');
+
+              if (this.currentRoom) {
+                return Guides.find({
+                  _id: {
+                      $in: this.currentRoom.childrenGuideIds
+                  }
+                });
+              }
             }
           });
-
-
+          
+          
           /* SUBSCRIPTIONS */
           this.subscribe('cms');
-
+          this.subscribe('guides');
+          
           /* AUTORUN*/
           this.autorun(() => {
 
           }); //autorun
-
+          
           /* FUNCTIONS */
+          this.resetNewGuide = function() {
+            this.newGuide = {
+              'cmsType': 'guide',
+              'guideType': 'blog', // for now, hard code...
+              'publish': 'preview',
+              'childrenArticleIds': [],
+              'title': '',
+              'adminDesc': ''
+              // Other data added when inserting into DB
+            };
+          }
+
           this.addNewGuide = function() {
+            this.resetNewGuide();
+            
             $('#new-guides').append(
-              $compile("<guide-create room='ra.room'></guide-create>")($scope)
+              $compile(`
+              <tr class="injected-table-view">
+                <td> <a href="#" editable-text="ra.newGuide.title" e-label="Guide Title">{{ ra.newGuide.title || 'Guide Name' }}</a> </td>
+                <td> <a href="#" editable-text="ra.newGuide.adminDesc" e-label="Guide Description">{{ ra.newGuide.adminDesc || 'Guide Description' }}</a> </td>
+                <td> 
+                  <a href="#" editable-select="ra.newGuide.publish" e-ng-options="s.value as s.text for s in ra.xEditableVisStatuses" buttons="no">
+                    {{ ra.showXEditableVisStatus() }}
+                  </a>
+                </td>
+                <td> - </td>
+                <td> <button type="button" class="btn btn-primary" ng-click="ra.submitNewGuide($event)" ng-disabled="!ra.canSubmitNewGuideFn()">Save</button> </td> 
+                <td> - </td>
+                <td> - </td>
+                <td> <i class="fa fa-times" aria-hidden="true" style="color:red;cursor:pointer" ng-click="ra.removeNewGuide($event)"></i> </td>
+              </tr>`
+              )($scope)
             );
           }
 
+          this.canAddNewGuide = function() {
+            this.getReactively('newGuide');
+            return _.size(this.newGuide) !== 0;
+          }
+
+          this.editGuide = function(guideId) {
+            window.location.href = '/guide/' + guideId + '/admin';
+          }
+          
+          this.deleteGuide = function() {
+            if (this.guideIdToDelete !== -1) {
+              this.call('deleteGuide', this.guideIdToDelete, (err) => {
+                if (err) {
+                  alert('Something went wrong deleting a guide: ' + err);
+                }
+              });
+              
+              this.call('updateParentGuideRoomWithDeletedChildGuideId', this.guideId, this.roomId, (err) => {
+                if (err) {
+                  alert('Something went wrong trying to update the parent guide room: ' + err);
+                }
+              });
+            } else {
+              alert('guide ID not found, cannot delete. Reseting guide ID.');
+            }
+
+            this.guideIdToDelete = -1;
+          }
+ 
+          this.submitNewGuide = function($event) {
+            this.call('createGuide', this.newGuide, (err, data) => {
+              if (err) {
+                alert('Something went wrong adding a new guide to database: ' + err);
+              } else {
+                this.call('updateParentGuideRoomWithChildGuideId', data, this.currentRoom._id, (err) => {
+                  if (err) {
+                    alert('Something went wrong trying to update the parent guide room: ' + err);
+                  }
+                });
+              }
+            });
+
+            this.removeNewGuide($event);
+          }
+
+          this.removeNewGuide = function($event) {
+            $event.target.closest('tr').remove();
+            this.newGuide = {};
+          }
+
+          this.canSubmitNewGuideFn = function() {
+            // check that guide has require properties
+            return this.newGuide.title && this.newGuide.adminDesc;
+          };
+
+          this.showXEditableVisStatus = function() {
+            selected = _.where(this.xEditableVisStatuses, { value: this.newGuide.publish });
+            return (this.newGuide.publish && selected.length) ? selected[0].text : 'Not set';
+          }
+
           /* INITIALIZE */
-          this.room = {};
+          this.guideIdToDelete = -1;
+
+          this.newGuide = {};
+          this.xEditableVisStatuses = [
+            { value: 'publish', text: 'Publish'},
+            { value: 'preview', text: 'Preview'},
+            { value: 'hide', text: 'Hide'}
+          ]
 
       } // controller
   };  //return
